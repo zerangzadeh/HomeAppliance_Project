ac7d6f53175c400c024ef015d027425cdd54d1e1
zerangzadeh <a.zerangzadeh@gmail.com>
2022-04-28 12:06:41 +0430

    Footer & Menu Section Moved to View Component

---
+++ HomeApplianceProject/ServiceHost/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.ViewComponents
+{
+    public class FooterViewComponent:ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
